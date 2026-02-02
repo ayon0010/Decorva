f09f9203db326387c405dd9da0acca93d7635497
ayon008 <shariar.ayon128@gmail.com>
2026-02-02 20:24:33 +0600

    prodcut-homepage-added

--- src/Shared/Home/Products.tsx
+++ src/Shared/Home/Products.tsx
@@ -2,10 +2,23 @@
 import React, { useState } from 'react'
 import { lora } from '../font/Rubik';
 import ProductCard from '../Card/ProductCard';
+import { useQuery } from '@tanstack/react-query';
+import ProductLoader from '@/app/(Main)/Loader/ProductLoader';
 
 const Products = () => {
 
     const [active, setActive] = useState(0);
+
+    const category = active === 0 ? 'plant-stands-and-movers' : active === 1 ? 'plant-families' : 'outdoor-plant-pots';
+
+    const { data: categoryProducts = [], isLoading } = useQuery({
+        queryKey: ['products', category],
+        queryFn: async () => {
+            const response = await fetch(`/api/category/${category}`);
+            const data = await response.json();
+            return data.category?.products;
+        }
+    })
 
 
     return (
@@ -15,8 +28,18 @@ const Products = () => {
                 <li className={`uppercase text-sm leading-[24px] border-b-2 font-normal hover:text-primary transition-all duration-300 cursor-pointer ${lora.className} ${active === 1 ? "border-b-primary text-primary" : "text-black border-b-transparent hover:border-b-primary"}`} onClick={() => setActive(1)}>Plant Families</li>
                 <li className={`uppercase text-sm leading-[24px] border-b-2 font-normal hover:text-primary transition-all duration-300 cursor-pointer ${lora.className} ${active === 2 ? "border-b-primary text-primary" : "text-black border-b-transparent hover:border-b-primary"}`} onClick={() => setActive(2)}>Outdoor Plant Pots</li>
             </ul>
-            <div className='mt-6'>
-                {/* <ProductCard /> */}
+            <div className='mt-6 grid 2xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-2 grid-cols-1 items-start w-full gap-6'>
+                {
+                    isLoading ? (
+                        Array.from({ length: 8 }).map((_, index) => (
+                            <ProductLoader key={index} />
+                        ))
+                    ) : (
+                        categoryProducts && categoryProducts.length > 0 && categoryProducts.slice(0, 8).map((product: { id: string, images: { url: string }[], name: string, price: number, regularPrice: number, salePrice: number, slug: string }) => (
+                            <ProductCard key={product.id} product={product} />
+                        ))
+                    )
+                }
             </div>
         </div>
     )
