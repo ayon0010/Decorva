bed317090318d662bf7b5271b57892e94bf1aac0
ayon0010 <shariarayon.freelancer@gmail.com>
2026-02-09 01:54:29 +0600

    fixed

--- next.config.ts
+++ next.config.ts
@@ -1,28 +1,10 @@
 import type { NextConfig } from "next";
-import path from "path";
-
-const floatingUiDir = path.dirname(require.resolve("@floating-ui/dom/package.json"));
-const floatingUiEsm = path.join(floatingUiDir, "dist", "floating-ui.dom.esm.js");
 
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   images: {
     domains: ['i.ibb.co'], // Add your external domain here
-  },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.alias = {
-        ...config.resolve?.alias,
-        "@floating-ui/dom": floatingUiEsm,
-      };
-    }
-    return config;
-  },
-  turbopack: {
-    resolveAlias: {
-      "@floating-ui/dom": floatingUiEsm,
-    },
   },
 };
 
